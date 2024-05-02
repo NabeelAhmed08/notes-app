864fb71e0b1dd7ca9f7ed6598205d42b6f5c137d
Nabeel Ahmed Mohammad <nabeelahmed08pro@gmail.com>
2024-05-02 16:46:48 -0500

    added CSS and made few initial changes in app.tsx

--- notes-app-ui/src/App.tsx
+++ notes-app-ui/src/App.tsx
@@ -1,25 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [notes,setNotes] = useState([
+    {
+      id: 1,
+      title: "note title 1",
+      content: "content 1"
+    },
+    {
+      id: 2,
+      title: "note title 2",
+      content: "content 2"
+    },{
+      id: 3,
+      title: "note title 3",
+      content: "content 3"
+    },
+    {
+      id: 4,
+      title: "note title 4",
+      content: "content 4"
+    }
+  ]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          NOTES APP UI
-        </a>
-      </header>
+
+    <div className="app-container">
+      <form className="note-form">
+        <input type="text" placeholder='title' required />
+        <textarea  placeholder='Content' rows={10} required></textarea>
+        <button type="submit">Add Note</button>
+        </form>
+    
+  <div className="notes-grid">
+    <div className="note-item">
+      <div className="notes-header">
+        <button>X</button>
+      </div>
+      <h2>Node Title</h2>
+        <p>Note Content</p>
     </div>
+  </div>
+  </div>
   );
 }
 
